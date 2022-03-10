581013286ed9035281435f1acb706d28575908b9
madhavikdm <100829584+madhavikdm@users.noreply.github.com>
2022-03-10 11:10:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-// load the http module
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-// configure our HTTP server
-var server = http.createServer(function (request, response) {
- var url = request.url;
-if(url ==='/about') {
-   response.write(' Welcome to about us page'); 
-    response.end(); 
- }
-//   response.writeHead(200, {"Content-Type": "text/plain"});
-//   response.end("Hello getintodevops.com\n");
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-// listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
