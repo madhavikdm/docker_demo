121c630633d0ad839f5d2acb3ca888300d4cc616
madhavikdm <100829584+madhavikdm@users.noreply.github.com>
2022-03-14 11:08:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
  const express = require('express')
 const app = express()
 const port = 3000
+const HOST = '0.0.0.0'
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port,host) => {
     console.log(`Example app listening on port ${port}`)
 })
