312e5eef3670a00347e55b149d041ce4f4e29ef2
Shweta Mane <shweta.mane@bnt-soft.com>
2022-07-07 15:53:01 +0530

    hello bnt 7878

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
- const express = require('express')
+const express = require('express')
 const app = express()
-const port = 3000
-const HOST = '0.0.0.0'
+const port = 7878 
+const HOST = '172.31.21.97'
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world-----today is 7th july 2022 updating service from jenkins build')
+app.get('/ping', (req, res) => {
+    console.log('Hello BNT SOFT');
+    res.send('Hello BNT SOFT')
 })
 
 app.listen(port,() => {
