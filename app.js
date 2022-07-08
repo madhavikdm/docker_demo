f98cc5d55d0a83bc9b1c5bc80fc31a97a0e2a75f
madhavikdm <100829584+madhavikdm@users.noreply.github.com>
2022-07-08 11:16:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const HOST = '0.0.0.0'
 
 
 app.get('/', (req, res) => {
-    res.send('Hello team.........welcome to Demo for deployment procedure')
+    res.send('Hello team.........now we updated service from jenkins build')
 })
 
 app.listen(port,() => {
